4482b3b71db77083196a13e2186b3de991265072
dsk1350 <dsk1350@gmail.com>
2019-05-25 04:17:59 +0000

    課題1-8

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,16 @@
 class TweetsController < ApplicationController
   def index
+    @tweets = Tweet.all.order(created_at: "desc")
+  end
+  def new
+    
+  end
+  def create
+    Tweet.create(tweet_params)
+  end
+  
+  private
+  def tweet_params
+    params.permit(:tweet_text)
   end
 end
